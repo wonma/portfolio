6415b2292337eb30cebad1e00240918a4ae3a187
Wonmi <wonmadesign@gmail.com>
2019-07-04 21:58:18 -0500

    Set up css file structure and style webview header

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const gulp = require('gulp'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
     cssImport = require('postcss-import'),
+    mixins = require('postcss-mixins'),
+	hexrgba = require('postcss-hexrgba'),
     browserSync = require('browser-sync').create();
 
 gulp.task('watch', function (cb) {
@@ -15,14 +17,15 @@ gulp.task('watch', function (cb) {
         }
     });
     
-    gulp.watch('./app/index.html', function() {
+    gulp.watch('./app/index.html', function(cd) {
         browserSync.reload();
+        cd();
     });
 
     function styles() {
         console.log('css change detected')
         return gulp.src('./app/assets/styles/styles.css')
-            .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+            .pipe(postcss([cssImport, mixins, hexrgba, cssvars, nested, autoprefixer]))
             .pipe(gulp.dest('./app/temp/styles'))
     }
 
@@ -34,7 +37,7 @@ gulp.task('watch', function (cb) {
     gulp.watch('./app/assets/styles/**/*.css', gulp.series(styles, renewCSS),
         function(cd) { 
             console.log('detected css')
-            cd(); } 
+        cd(); } 
     );
 
     cb();
