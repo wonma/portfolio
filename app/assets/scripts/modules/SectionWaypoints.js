c27346e0a1a275580acbcfc24a424cfffebb6900
Wonmi <wonmadesign@gmail.com>
2019-07-14 14:45:28 -0500

    Finished on-scroll dynamic header in accordance with secondary navigation(dynamic header burger nav inactive for now)

--- app/assets/scripts/modules/SectionWaypoints.js
+++ app/assets/scripts/modules/SectionWaypoints.js
@@ -4,8 +4,43 @@ import waypoints from 'waypoints/lib/noframework.waypoints.min';
 class SectionWaypoints {
     constructor() {
         this.pageSections = $('.myself-section');
-        this.sectionLinks = $('.secondary-nav a')
+        this.secondaryNav = $('.secondary-nav');
+        this.sectionLinks = $('.secondary-nav a');
+        this.header = $('.site-header--dynamic');
+        this.headerTriggerPoint = $('.hero__heading--sub');
         this.colorTheLink();
+        this.headerControl();
+    }
+
+    headerControl() {
+        var that = this;
+        var prevScrollpos = window.pageYOffset;
+        window.onscroll = function () {
+            var currentScrollPos = window.pageYOffset;
+            if (prevScrollpos > currentScrollPos && currentScrollPos > 200) { /* 이전스크롤위치가 현재스크롤위치보다 아래이면 */
+                that.header[0].style.top = "0px"; /* 나오기 */
+                that.secondaryNav[0].style.top ="120px";
+            } else  {
+                that.header[0].style.top = "-120px"; /* 숨기 */
+                that.secondaryNav[0].style.top = "0px";
+
+            }
+            prevScrollpos = currentScrollPos;
+        }
+
+        new Waypoint({
+            element: that.headerTriggerPoint[0],
+            handler: function(direction){
+                if(direction =="up") {
+                    $(that.header).addClass("hide-dynamic-header");
+                }
+                 else {
+                    $(that.header).removeClass("hide-dynamic-header");
+                }
+            },
+            offset: "-10%"
+
+        })    
     }
 
     colorTheLink() {
@@ -40,5 +75,6 @@ class SectionWaypoints {
         })
     }
 }
+
 
 export default SectionWaypoints;
