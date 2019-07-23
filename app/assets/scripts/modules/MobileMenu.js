2e9fadc18d232f1e7a403a0779933e49a9ef40b3
Wonmi <wonmadesign@gmail.com>
2019-07-23 01:52:10 -0500

    Fixed hamburger menu and Finished basic structure of hero of portfolio single page.

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,18 +1,33 @@
 class MobileMenu {
     constructor() {
-        this.hamburgerIcon = document.querySelector('.hamburger');
+        this.hamburgerIcon = document.querySelectorAll('.hamburger');
         this.overlayMenu = document.querySelector('.overlay-nav');
+        this.closeIcon = document.querySelector('.icon__close');
         this.events();
     }
 
     events() {
-        this.hamburgerIcon.addEventListener('click', this.toggleTheMenu.bind(this));
+        this.hamburgerIcon[0].addEventListener('click', this.openMenu.bind(this));
+        this.hamburgerIcon[1].addEventListener('click', this.openMenu2.bind(this));
+        this.closeIcon.addEventListener('click', this.closeMenu.bind(this));
     }
 
-    toggleTheMenu() {
-        console.log('clicked')
-        this.overlayMenu.classList.toggle('overlay-nav--is-open');
-        this.hamburgerIcon.classList.toggle('hamburger--is-close-icon');
+    openMenu() {
+        this.overlayMenu.classList.add('overlay-nav--is-open'); 
+        this.closeIcon.classList.add('icon__close--is-on');
+    }
+
+    openMenu2() {
+        this.overlayMenu.classList.add('overlay-nav--is-open');
+        this.closeIcon.classList.add('icon__close--is-on-at-dynamic');
+    }
+
+    closeMenu() {
+        this.overlayMenu.classList.remove('overlay-nav--is-open');
+        this.hamburgerIcon[0].classList.remove('hamburger--is-close-icon');
+        this.hamburgerIcon[1].classList.remove('hamburger--is-close-icon');
+        this.closeIcon.classList.remove('icon__close--is-on');
+        this.closeIcon.classList.remove('icon__close--is-on-at-dynamic');
     }
 }
 
