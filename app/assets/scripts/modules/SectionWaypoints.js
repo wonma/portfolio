9e7bda0a2be3aeff4a7a36d53a2c9edfc2517360
Wonmi <wonmadesign@gmail.com>
2019-07-23 15:31:58 -0500

    Fixed a few class names for button, section

--- app/assets/scripts/modules/SectionWaypoints.js
+++ app/assets/scripts/modules/SectionWaypoints.js
@@ -3,7 +3,7 @@ import waypoints from 'waypoints/lib/noframework.waypoints.min';
 
 class SectionWaypoints {
     constructor() {
-        this.pageSections = $('.myself-section');
+        this.pageSections = $('.anchored-section');
         this.secondaryNav = $('.secondary-nav');
         this.sectionLinks = $('.secondary-nav a');
         this.header = $('.site-header--dynamic');
@@ -18,11 +18,14 @@ class SectionWaypoints {
             var currentScrollPos = window.pageYOffset;
             if (prevScrollpos > currentScrollPos && currentScrollPos > 400) { /* 이전스크롤위치가 현재스크롤위치보다 아래이면 */
                 that.header[0].style.top = "0px"; /* 나오기 */
-                that.secondaryNav[0].style.top ="50px";
+                if (that.secondaryNav[0]) {
+                    that.secondaryNav[0].style.top = "50px";
+                }
             } else  {
                 that.header[0].style.top = "-60px"; /* 숨기 */
-                that.secondaryNav[0].style.top = "0px";
-
+                if (that.secondaryNav[0]) {
+                    that.secondaryNav[0].style.top = "0px";
+                }
             }
             prevScrollpos = currentScrollPos;
         }
@@ -30,36 +33,40 @@ class SectionWaypoints {
 
     colorTheLink() {
         var theObject = this;
-        this.pageSections.each(function(){
-            var eachSection = this;
 
-            new Waypoint({
-                element: eachSection,
-                handler: function(direction){
-                    if (direction == "down") {
-                        var matchingLinkInfo = eachSection.getAttribute("data-matching-link");
-                        theObject.sectionLinks.removeClass("is-current-link");
-                        $(matchingLinkInfo).addClass("is-current-link");
+        if (this.pageSections) {
+            this.pageSections.each(function () {
+                var eachSection = this;
+
+                new Waypoint({
+                    element: eachSection,
+                    handler: function (direction) {
+                        if (direction == "down") {
+                            var matchingLinkInfo = eachSection.getAttribute("data-matching-link");
+                            theObject.sectionLinks.removeClass("is-current-link");
+                            $(matchingLinkInfo).addClass("is-current-link");
+                        }
+                    },
+                    offset: 80
+                })
+
+                new Waypoint({
+                    element: eachSection,
+                    handler: function (direction) {
+                        if (direction == "up") {
+                            var matchingLinkInfo = eachSection.getAttribute("data-matching-link");
+                            theObject.sectionLinks.removeClass("is-current-link");
+                            $(matchingLinkInfo).addClass("is-current-link");
+                        }
+                    },
+                    offset: function () {
+                        return -(this.element.clientHeight - 400);
                     }
-                },
-                offset: 80
-            })
+                })
 
-            new Waypoint({
-                element: eachSection,
-                handler: function (direction) {
-                    if (direction == "up") {
-                        var matchingLinkInfo = eachSection.getAttribute("data-matching-link");
-                        theObject.sectionLinks.removeClass("is-current-link");
-                        $(matchingLinkInfo).addClass("is-current-link");
-                    }
-                },
-                offset: function () {
-                    return -(this.element.clientHeight-400);
-                }
             })
+        }
 
-        })
     }
 }
 
