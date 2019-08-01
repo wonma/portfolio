ef0a150cbc64290115224d2f8cf2b78be9aeb2aa
Wonmi <wonmadesign@gmail.com>
2019-07-31 22:17:26 -0500

    Finished structure of visual strategy section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -7,9 +7,11 @@ import MobileMenu from './modules/MobileMenu';
 import HeroSlider from './modules/HeroSlider';
 import ScrollToID from './modules/ScrollToID';
 import SectionWaypoints from './modules/SectionWaypoints';
+import DoubleSlider from './modules/DoubleSlider';
 
 // Instantiate a new object using our modules/classes
 var heroSlider = new HeroSlider();
 var mobileMenu = new MobileMenu();
 var scrollToID = new ScrollToID();
 var sectionWaypoints = new SectionWaypoints();
+var doubleSlider = new DoubleSlider();
