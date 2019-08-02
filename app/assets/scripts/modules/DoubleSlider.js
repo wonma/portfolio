ab8e758bb7b5b3e99995410383ca4163abec995e
Wonmi <wonmadesign@gmail.com>
2019-08-01 22:37:44 -0500

    Added styling to DoubleSlider(50%)

--- app/assets/scripts/modules/DoubleSlider.js
+++ app/assets/scripts/modules/DoubleSlider.js
@@ -4,6 +4,7 @@ class DoubleSlider {
     constructor() {
         this.sliderImage = $(".slider-image");
         this.sliderContent = $(".slider-content");
+        this.status = $('.pagingInfo');
         this.initDoubleSlider();
     }
 
@@ -12,16 +13,25 @@ class DoubleSlider {
             slidesToShow: 1,
             slidesToScroll: 1,
             arrows: false,
-            asNavFor: '.slider-content'
+            asNavFor: '.slider-content',
+            autoplaySpeed: 2000,
+            autoplay: true
+        });
+
+        var theStatus = this.status;
+        this.sliderContent.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+            //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+            var i = (currentSlide ? currentSlide : 0) + 1;
+            theStatus.text(i + '/' + slick.slideCount);
         });
 
         this.sliderContent.slick({
             slidesToShow: 1,
             slidesToScroll: 1,
             asNavFor: '.slider-image',
-            dots: true,
             arrows: true,
             fade: true,
+            // autoplay: true,
             focusOnSelect: true
         })
     }
