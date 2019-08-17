4806b23a7ea9f73a41399e37f83e374808352407
Wonmi <wonmadesign@gmail.com>
2019-08-17 00:52:51 -0500

    Added styling to quick nav of design page

--- app/assets/scripts/modules/QuickNav.js
+++ app/assets/scripts/modules/QuickNav.js
@@ -3,13 +3,18 @@ class QuickNav {
         this.quickNavBtn = document.querySelector('.quickNavBtn');
         this.overlayQuickNav = document.querySelector('.overlayQuickNav');
         this.closeIconQuickNav = document.querySelector('.closeIconQuickNav');
+        this.scrim = document.querySelector('.overlay-scrim');
         this.events();
         console.log('constructor read');
     }
 
     events() {
         this.quickNavBtn.addEventListener('click', this.openQuickNav.bind(this));
+        this.quickNavBtn.addEventListener('click', this.openScrim.bind(this));
+
         this.closeIconQuickNav.addEventListener('click', this.closeQuickNav.bind(this));
+        this.closeIconQuickNav.addEventListener('click', this.closeScrim.bind(this));
+
         console.log('click event registered');
     }
 
@@ -18,8 +23,13 @@ class QuickNav {
         console.log(this.overlayQuickNav);
     }
 
+    openScrim() {
+        this.scrim.classList.add('scrim-on');
+    }
+
     closeQuickNav() {
         this.overlayQuickNav.classList.remove('quick-nav--is-open');
+        this.scrim.classList.remove('scrim-on');
     }
 }
 
